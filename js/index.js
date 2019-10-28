104d163d240aab9d4267dfc2db60c0ffa321fb4e
Jonathan Mendes <jonathan_mendes96@outlook.com>
2019-10-28 17:41:27 -0300

    Alterações Gerais

---
+++ js/index.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
