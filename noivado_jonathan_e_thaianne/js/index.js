1ab343517daa811e5d1195af15a5524f319d164c
jonathan-mendes <jonathan_mendes96@outlook.com>
2020-07-08 22:19:56 -0300

    Alterações

---
+++ noivado_jonathan_e_thaianne/js/index.js
@@ -0,0 +1,10 @@
+function confirmado() {
+    let section_1 = document.getElementById("section_1");
+    let section_2 = document.getElementById("section_2");
+
+    section_1.style.opacity = 0;
+    section_1.style.position = 'absolute';
+    
+    section_2.style.opacity= 1;
+    section_2.style.top = 0;
+}
